42b28c93d04b5a41896661ab03f5cd7f2063b3d6
CuauCabrera <cabrera.cuau@gmail.com>
2023-11-22 20:58:57 -0600

    Add cors policy middleware

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,21 @@ import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
 
 //Add Middleware for parsing request body as JSON
 app.use(express.json());
+
+//Add Middleware for CORS policy
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
 
 app.get('/', (request, response) => {
     console.log(request)
